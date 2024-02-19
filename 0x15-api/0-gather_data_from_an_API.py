a6a7afd40012d281407bdb01b44be1f0bebf7161
Julius-3367 <korirjulius001@gmail.com>
2024-02-19 07:55:55 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Returns to-do list information for a given employee ID.
+"""
+
+
 from requests import get
 import sys
 
